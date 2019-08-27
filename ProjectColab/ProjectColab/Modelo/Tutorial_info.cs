2f009479350cb88452fe11c6dccb34d9e6a1ec49
Luenne Mafra <40248856+EnneS2@users.noreply.github.com>
2019-08-27 11:36:15 -0300

    Finalizando adição de arquivo no EditTutorial

---
+++ ProjectColab/ProjectColab/Modelo/Tutorial_info.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectColab.Modelo
+{
+    public class Tutorial_info
+    {
+        public string tutorial_id { get; set; }
+        public byte[] logo { get; set; }
+        public string pr_info { get; set; }
+
+        public Tutorial_info()
+        {
+            this.tutorial_id = "";
+            this.logo = null;
+            this.pr_info = "";
+        }
+        public Tutorial_info(string atutorial_id, byte[] alogo, string apr_info)
+        {
+            this.tutorial_id = atutorial_id;
+            this.logo = alogo;
+            this.pr_info = apr_info;
+        }
+    }
+
+}
