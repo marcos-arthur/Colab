5ec930b1e82b718277eba7cb63c0ed21e8a9d4b2
vitoria medeiros <53539926+vitoriasmedeiros@users.noreply.github.com>
2019-08-28 17:14:40 -0300

    tela de login

---
+++ ProjectColab/ProjectColab/WebFormLogin.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormLogin.aspx.cs" Inherits="ProjectColab.WebFormLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link rel="stylesheet" type="text/css" href="style.css"/>  
+<link rel="stylesheet" type="text/css" href="styleadd.css"/> 
+    <title>ABRIR CHAMADO</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+	        <div class="logo"><a href="index.aspx">COLAB</a></div>
+            
+            <div class="mid2">
+               
+                <p>Login</p><asp:TextBox ID="Login" runat="server" CssClass="text"></asp:TextBox>
+                <p>Senha</p><asp:TextBox ID="Senha" TextMode="Password" runat="server" CssClass="text"></asp:TextBox>
+                <asp:TextBox ID="data" runat="server" style="display:none" CssClass="text"></asp:TextBox>
+                <br />
+                <br />
+                <asp:Button ID="add" runat="server" Text="Entrar"   CssClass="cancelbtn"/>
+            </div>       
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProjectColab/ProjectColab/WebFormLogin.aspx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ProjectColab {
+    
+    
+    public partial class WebFormLogin {
+        
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Controle Login.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Login;
+        
+        /// <summary>
+        /// Controle Senha.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Senha;
+        
+        /// <summary>
+        /// Controle data.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox data;
+        
+        /// <summary>
+        /// Controle add.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button add;
+    }
+}
