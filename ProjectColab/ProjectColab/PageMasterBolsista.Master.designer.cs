94d752086101e1cde591c9d0a80a354895e9aa64
Enne Mafra <40248856+EnneS2@users.noreply.github.com>
2019-10-14 14:58:29 -0300

    Fazendo umas baguncinhas aí

--- ProjectColab/ProjectColab/PageMasterBolsista.Master.designer.cs
+++ ProjectColab/ProjectColab/PageMasterBolsista.Master.designer.cs
@@ -31,15 +31,6 @@ namespace ProjectColab {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Controle Button1.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-        
-        /// <summary>
         /// Controle ContentPlaceHolder1.
         /// </summary>
         /// <remarks>
