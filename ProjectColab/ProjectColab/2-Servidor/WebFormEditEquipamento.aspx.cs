cfb680d05ce97107a515989afcfdc33e4e32bcb5
Marcos Arthur <31050100+Pas0quit0@users.noreply.github.com>
2019-10-23 11:48:56 -0300

    EditEquipamento atualizado com mensagens de erro

--- ProjectColab/ProjectColab/2-Servidor/WebFormEditEquipamento.aspx.cs
+++ ProjectColab/ProjectColab/2-Servidor/WebFormEditEquipamento.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,24 +13,57 @@ namespace ProjectColab._2_Servidor
         protected void Page_Load(object sender, EventArgs e)
         {
             string id = Session["idequip"].ToString();
+
+            Label errorModelo = null;
+            Label errorQuant = null;
+
+            if (LabEquips.Items.Count > 0)
+            {
+                errorModelo = (Label)LabEquips.Items[0].FindControl("MsgErroModelo");
+                errorQuant = (Label)LabEquips.Items[0].FindControl("MsgErroQuant");
+
+                errorModelo.Text = "";
+                errorQuant.Text = "";
+            }            
         }
 
         protected void Button2_Click(object sender, EventArgs e)
         {
+
+            Label errorModelo = (Label)LabEquips.Items[0].FindControl("MsgErroModelo");
+            Label errorQuant = (Label)LabEquips.Items[0].FindControl("MsgErroQuant");
+
+            bool ok = true;
+
             DAL.DALEquipamento dalEquip = new DAL.DALEquipamento();
             Modelo.Equipamento equip = dalEquip.Select(Session["idequip"].ToString());
 
             TextBox quantidade = (TextBox)LabEquips.Items[0].FindControl("quant");
 
-            equip.quantidade = Convert.ToInt32(quantidade.Text);
+            if (int.TryParse(quantidade.Text, out int result)) equip.quantidade = Convert.ToInt32(quantidade.Text);
+            else equip.quantidade = 0;
 
             TextBox modelo = (TextBox)LabEquips.Items[0].FindControl("modelo");
 
             equip.modelo = modelo.Text;
 
-            dalEquip.Update(equip);
+            try
+            {
+                dalEquip.Update(equip);
+            }
+            catch (SqlException error)
+            {
+                ok = false;
 
-            Response.Redirect("~//2-Servidor/WebFormCRUDEquipamento.aspx");
+                if (error.Message.Contains("O equipamento inserido deve pertencer a alguma sala")) Session["MsgErroDropSalas"] = "O equipamento inserido deve pertencer a alguma sala";
+
+                if (error.Message.Contains("A quantidade de equipamento deve ser um número maior que 0")) errorModelo.Text = "A quantidade de equipamento deve ser um número maior que 0";
+
+                if (error.Message.Contains("O modelo do equipamento nao pode ser vazio")) errorQuant.Text = "O modelo do equipamento nao pode ser vazio";
+            }
+
+           /* if (ok) Response.Redirect("~//2-Servidor/WebFormCRUDEquipamento.aspx");
+            else Response.Redirect("~//2-Servidor/WebFormEditEquipamento.aspx");     */       
         }
         protected void Button3_Click(object sender, EventArgs e)
         {
